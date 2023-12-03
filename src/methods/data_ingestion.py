e2d6e42b3b3d7accc26416232cfd430d85887f04
snguyen98 <snguyen.cs@outlook.com>
2023-12-03 11:57:54 +0000

    Added documentation

--- src/methods/data_ingestion.py
+++ src/methods/data_ingestion.py
@@ -5,19 +5,23 @@ Contains functions to ingest data from a file and format it
 """
 
 __date__ = "2023-11-10"
-__authors__ = "NedeeshaWeerasuriya"
+__authors__ = "NedeeshaWeerasuriya, Sang Nguyen"
 __version__ = "0.1"
 
-from enum import Enum
-
+"""
+Import Modules
+"""
 import numpy as np
 import pandas as pd
 import requests
 
+from enum import Enum
 from src.helpers.time_tracker import track_time
 from src.helpers.utils import is_number
 
-# Enums
+"""
+Enums
+"""
 Request = Enum('Request', ['GET','POST'])
 
 @track_time
@@ -57,6 +61,20 @@ def filter_data_ranges(data: pd.DataFrame, data_range: tuple) -> pd.DataFrame:
 
 @track_time
 def call_api(url: str, headers: dict = None, params: dict = None, request: Request = Request.GET):
+    """
+    Performs a REST API call and returns the response
+
+    Params
+    ------
+    url: str
+        The request url
+    headers: dict
+        A dictionary of request headers
+    params: dict
+        A dictionary of parameters in the request body
+    request: Request
+        The REST method used in the API call, must be a valid item in the Request Enum
+    """
     try:
         if request == Request.GET:
             response = requests.get(url, headers=headers, params=params)
