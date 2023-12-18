2ad6cefe4ddbae1cf645f23f71da4ae632f22270
snguyen98 <snguyen.cs@outlook.com>
2023-12-18 15:42:33 +0000

    Moved work from amadeus_test to separate py file

---
+++ src/amadeus_api.py
@@ -0,0 +1,74 @@
+"""
+Data Ingestion Script for Amadeus API
+
+"""
+
+__date__ = "2023-12-03"
+__author__ = "Sang Nguyen"
+__version__ = "0.1"
+
+
+# %% --------------------------------------------------------------------------
+# Import Modules
+# -----------------------------------------------------------------------------
+import sys
+sys.path.append('../')
+
+import json
+from enum import Enum
+from src.methods.data_ingestion import call_api, Request
+
+# Enums
+Env = Enum('Env', ['DEV','PROD'])
+
+
+# %% --------------------------------------------------------------------------
+# Main Script
+# -----------------------------------------------------------------------------
+def default_parameters() -> dict:
+    request_params = {
+        "latitude": "51.484703",
+        "longitude": "-0.061048",
+        "radius": "5",
+        "categories": "SIGHTS"
+    }
+
+    return request_params
+
+def get_auth_token(env: Env = Env.DEV) -> str:
+    env_str = 'prod' if env == Env.PROD else 'dev'
+
+    with open("config.json","r") as config_file:
+        config = json.load(config_file)['amadeus_api']['auth'][env_str]
+
+    url = config['url']
+
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    request_params = {
+        "grant_type": "client_credentials",
+        "client_id": config['client_id'],
+        "client_secret": config['client_secret']
+    }
+
+    res = call_api(url=url, headers=headers, params=request_params, request=Request.POST)
+    
+    return res['access_token']
+
+def amadeus_pipeline(url: str, request_params: dict, env: Env = Env.DEV):
+    """
+    Pipeline for the Amadeus API
+    """
+
+    auth_token = get_auth_token(env)
+
+    headers = {
+        "Authorization": f"Bearer {auth_token}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    res = call_api(url=url, headers=headers, params=request_params, request=Request.GET)
+
+    return res
