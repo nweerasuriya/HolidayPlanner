a6691e215f1d23f9849a57f07cfa03aff4492e81
Ned <nedeeshaw@virginmedia.com>
2024-01-04 21:40:46 +0000

    Added relative distance function

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -7,6 +7,7 @@ __author__ = "NedeeshaWeerasuriya"
 
 import os
 
+import geopy.distance
 import numpy as np
 
 
@@ -45,3 +46,13 @@ def check_substr_in_dict(x: dict, substr: str) -> str:
         if substr in key:
             return x[key]
     return np.nan
+
+
+def relative_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
+    """
+    Calculate the relative distance between two points on the Earth's surface using geopy.
+    Uses the Vincenty distance which has more accurate ellipsoidal models, such as WGS-84.
+    """
+    coords_1 = (lat1, lon1)
+    coords_2 = (lat2, lon2)
+    return geopy.distance.distance(coords_1, coords_2).km
