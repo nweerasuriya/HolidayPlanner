d15fd46ecf5a223ea0dde6e5ce14d6bf454677cf
Ned <nedeeshaw@virginmedia.com>
2024-02-02 21:58:46 +0000

    add openweather api

--- src/methods/data_ingestion.py
+++ src/methods/data_ingestion.py
@@ -8,12 +8,14 @@ __date__ = "2023-11-10"
 __authors__ = "NedeeshaWeerasuriya"
 __version__ = "0.1"
 
-
+# Import required libraries
 import numpy as np
 import pandas as pd
 import requests
+from pyowm import OWM
 from pyproj import Proj, Transformer
 
+# Import helper functions
 from src.helpers.time_tracker import track_time
 from src.helpers.utils import is_number
 
@@ -49,7 +51,6 @@ def filter_data_ranges(data: pd.DataFrame, data_range: tuple) -> pd.DataFrame:
     data = data.loc[data.values >= data_range[0]]
     # Filter upper values
     data = data.loc[data.values <= data_range[1]]
-
     return data
 
 
@@ -74,7 +75,21 @@ def convert_easting_northing_to_long_lat(easting: int, northing: int) -> tuple:
     """
     inProj = Proj("epsg:27700")
     outProj = Proj("epsg:4326")
-    longitude, latitude = Transformer.from_proj(inProj, outProj).transform(
+    latitude, longitude = Transformer.from_proj(inProj, outProj).transform(
         easting, northing
     )
-    return longitude, latitude
+    return latitude, longitude
+
+
+@track_time
+def get_openweather(lat: float, lon: float, api_key="3201bd50938164da1d0f66147bde4f78"):
+    """
+    Get current weather and forecast for a location using OpenWeatherMap API
+    """
+    owm = OWM(api_key)
+    mgr = owm.weather_manager()
+    # Get current weather
+    observation = mgr.weather_at_coords(lat=lat, lon=lon)
+    # Get forecast
+    forecast = mgr.forecast_at_coords(lat=lat, lon=lon, interval="3h")
+    return observation, forecast
