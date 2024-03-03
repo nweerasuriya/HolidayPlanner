f638421e1c3c7d50cdf7af5de47e09417fa222de
snguyen98 <snguyen.cs@outlook.com>
2024-03-03 22:40:57 +0000

    Added comments

--- src/amadeus_api.py
+++ src/amadeus_api.py
@@ -24,7 +24,10 @@ from src.helpers.utils import relative_distance
 # %% --------------------------------------------------------------------------
 # Main Script
 # -----------------------------------------------------------------------------
-def default_parameters() -> dict:
+def default_params() -> dict:
+    """
+    Default parameters for the Amadeus API
+    """
     request_params = {
         "latitude": "51.484703",
         "longitude": "-0.061048",
@@ -34,24 +37,46 @@ def default_parameters() -> dict:
 
     return request_params
 
-def get_auth_token(config: dict) -> str:
-    url = config['auth_url']
+def get_auth_token(auth_url: str, client_id: str, client_secret: str) -> str:
+    """
+    Retrieves an authentication token from the Amadeus API
 
+    Params
+    ------
+    auth_url: str
+        The url used in the api call for authentication
+    client_id: str
+        The Client ID of the Amadeus API App
+    client_secret: str
+        The Client Secret of the Amadeus API App
+    """
     headers = {
         "Content-Type": "application/x-www-form-urlencoded"
     }
 
     request_params = {
         "grant_type": "client_credentials",
-        "client_id": config['client_id'],
-        "client_secret": config['client_secret']
+        "client_id": client_id,
+        "client_secret": client_secret
     }
 
-    res = call_api(url=url, headers=headers, params=request_params, request=Request.POST)
+    res = call_api(url=auth_url, headers=headers, params=request_params, request=Request.POST)
     
     return res['access_token']
 
 def format_amadeus_data(res_api: dict, param_lat: float, param_long: float) -> pd.DataFrame:
+    """
+    Retrieves an authentication token from the Amadeus API
+
+    Params
+    ------
+    res_api: dict
+        The resulting dictionary from the api call
+    param_lat: float
+        The latitude parameter supplied to the api call
+    param_long: float
+        The longitude parameter supplied to the api call
+    """
     df = pd.DataFrame(res_api['data'], columns=['name','category','geoCode','rank','tags'])
     df = pd.concat([df.drop(['geoCode'], axis=1),df['geoCode'].apply(pd.Series)], axis=1)
 
@@ -62,9 +87,16 @@ def format_amadeus_data(res_api: dict, param_lat: float, param_long: float) -> p
 
     return df
 
-def amadeus_pipeline(request_params: dict, isProd: bool = False):
+def amadeus_pipeline(request_params: dict=default_params(), isProd: bool = False):
     """
     Pipeline for the Amadeus API - Points of Interest
+
+    Params
+    ------
+    request_params: str
+        The parameters to be supplied to the api request
+    isProd: bool
+        A boolean defining if the api call should be to the prod environment
     """
     env_str = 'prod' if isProd else 'dev'
 
@@ -73,7 +105,7 @@ def amadeus_pipeline(request_params: dict, isProd: bool = False):
     with open(config_path,"r") as config_file:
         config = json.load(config_file)['amadeus_api'][env_str]
 
-    auth_token = get_auth_token(config=config)
+    auth_token = get_auth_token(config['auth_url'], config['client_id'], config['client_secret'])
 
     url = config['poi_url']
 
